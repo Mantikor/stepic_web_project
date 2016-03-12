b6774945f983218df5fe40b21f3cfdf6fcec477b
Sergey <mantikor@tut.by>
2016-03-12 19:58:11 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,5 +15,6 @@ urlpatterns = patterns('',
     url(r'^question/.+', 'qa.views.test'),
     url(r'^ask/', 'qa.views.test'),
     url(r'^popular', 'qa.views.test'),
-    url(r'^new', 'qa.views.test')
+    url(r'^new', 'qa.views.test'),
+    url(r'^question/5/', 'qa.views.q_answer')
 )
