54a1488bf65fcfa869fccba3b71962e6179c7af3
Sergey <mantikor@tut.by>
2016-03-12 21:12:29 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -7,14 +7,16 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'qa.views.list_question'),
-    url(r'^login/', 'qa.views.login'),
-    url(r'^signup', 'qa.views.signup'),
-    url(r'^question/(?P<slug>\w+)/$', 'qa.views.show_question'),
-    url(r'^ask/', 'qa.views.post_question'),
-    url(r'^popular/', 'qa.views.list_popular'),
-    url(r'^new/', 'qa.views.test'),
-    url(r'^answer/', 'qa.views.post_answer')
+    
+    url(r'^', include('qa.urls')),
+    
+    # url(r'^admin/', include(admin.site.urls)),
+    # url(r'^$', 'qa.views.list_question'),
+    # url(r'^login/', 'qa.views.login'),
+    # url(r'^signup', 'qa.views.signup'),
+    # url(r'^question/(?P<slug>\w+)/$', 'qa.views.show_question'),
+    # url(r'^ask/', 'qa.views.post_question'),
+    # url(r'^popular/', 'qa.views.list_popular'),
+    # url(r'^new/', 'qa.views.test'),
+    # url(r'^answer/', 'qa.views.post_answer')
 )
